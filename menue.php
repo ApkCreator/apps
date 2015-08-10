29121fff8174c5c6d489f0cde7457ea1dafed6cd
ApkCreator <Volkmar_S@web.de>
2015-08-10 20:48:31 +0200

    test
    test

---
+++ menue.php
@@ -0,0 +1,157 @@
+﻿<html>
+<head>
+
+<style type="text/css">
+.submenu{display: none;}
+.menuetitlel {  margin:.5em; padding:.5em; background-color:#CCCC66;}
+a:link { border:1px; outset gray; margin:.5em; padding:.5em;}
+a:hover { border:1px; outset gray; margin:.8em; padding:.8em; background-color:#efd; }
+</style>
+
+
+
+
+
+
+<script type="text/javascript">
+
+
+function Menue (obj){
+
+var a = document.getElementById(obj);
+
+if ( obj == "1") {
+		document.getElementById('2').style.display = "none";
+		document.getElementById('3').style.display = "none";
+		document.getElementById('4').style.display = "none";
+		if(a.style.display == "none"){a.style.display = "block";} else { a.style.display = "none";}
+}
+
+if ( obj == "2") {
+		document.getElementById('2').style.display = "none";
+		document.getElementById('3').style.display = "none";
+		document.getElementById('4').style.display = "none";
+		if(a.style.display == "none"){a.style.display = "block";} else { a.style.display = "none";}
+}
+
+
+if ( obj == "3") {
+		document.getElementById('2').style.display = "none";
+		document.getElementById('3').style.display = "none";
+		document.getElementById('4').style.display = "none";
+
+		if(a.style.display == "none"){a.style.display = "block";} else { a.style.display = "none";}
+}
+
+if ( obj == "4") {
+		document.getElementById('2').style.display = "none";
+		document.getElementById('3').style.display = "none";
+		document.getElementById('4').style.display = "none";
+		if(a.style.display == "none"){a.style.display = "block";} else { a.style.display = "none";}
+}
+
+			}
+
+</script>
+
+
+
+
+
+
+
+
+
+
+</head>
+
+
+
+
+
+
+<body bgcolor = #F5F5DC>
+ 
+    
+<div id="masterdiv">
+
+
+<div>
+<p>
+<br>
+</div>
+
+<div>
+<p>
+</div>
+
+<div class="menuetitlel" onmouseover="Menue('1')"><u>Grundkenntnisse</u></div>
+
+<span class="submenu" id="1">
+<hr noshade="noshade" size="1">
+  <a href="../buchstabieren/buchstabieren.html" target=Website>Das Buchstabieren</a><br>
+<hr noshade="noshade" size="1">
+  <a href="../frequenzen/frequenzen.html" target=Website>Frequenzen</a><br>
+<hr noshade="noshade" size="1">
+  <a href="../relai/relai.html" target=Website >Die Relaisstation</a><br>
+<hr noshade="noshade" size="1">          
+  <a href="../klasse_e/klasse_e.html" target=Website>Einsteigerklasse</a>
+<hr noshade="noshade" size="1">
+</span>
+
+</div>
+
+<div>
+<p>
+</div>
+
+<div class="menuetitlel" onmouseover="Menue('2')"><u>Aktivit&auml;ten</div>
+<span class="submenu" id="2">
+<hr noshade="noshade" size="1">
+<a href="../contest/contest.html" target=Website>&Uuml;bersicht Conteste</a>
+<hr noshade="noshade" size="1">
+<a href="../logbuch/log.html" target=Website>mein Logbuch</a>
+<hr noshade="noshade" size="1">
+</span>
+</div>   
+
+
+<div>
+<p>
+</div>
+
+<div class="menuetitlel" onmouseover="Menue('3')"><u>Verweise</div>
+<span class="submenu" id="3">
+<hr noshade="noshade" size="1">
+<a href="../verweise/verweise.html" target="_Parent">Linkliste</a>
+<hr noshade="noshade" size="1">
+<a href="../verweise/morsen.html" target="_Parent">rund ums Morsen</a>
+<hr noshade="noshade" size="1">
+<a href="http://www.qslonline.de/hamabcf.htm" target=Website><b>online Lexikon</b></a>
+<hr noshade="noshade" size="1">
+</span>
+</div>   
+
+
+
+<div>
+<p>
+</div>
+
+
+<div class="menuetitlel" onmouseover="Menue('4')"><u>Kontakt</u></div>
+<span class="submenu" id="4">
+<hr noshade="noshade" size="1">
+<a href="../gaestebuch/gaestebuch.php" target=Website>Zum G&auml;stebuch</a>
+<hr noshade="noshade" size="1">
+<a href="../impressum/impressum.html" target=Website >Impressum</a>
+<hr noshade="noshade" size="1">          
+</span>
+</div>
+
+
+
+    
+</body>
+</html>
+
